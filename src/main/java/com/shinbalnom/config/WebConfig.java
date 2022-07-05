58fdfe6286c43d3e9174300099ffb92eed45d97f
alexanderbyun <byun9110@gmail.com>
2022-07-05 14:50:04 -0400

    CORS update, new class WebConfig

--- src/main/java/com/shinbalnom/config/WebConfig.java
+++ src/main/java/com/shinbalnom/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.shinbalnom;
+package com.shinbalnom.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
